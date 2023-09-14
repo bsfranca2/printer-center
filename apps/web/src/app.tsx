76beebc868db86bbf5bcacc4eb5e0f3b6fe93db6
Bruno Franca <bsfranca2@gmail.com>
2023-09-13 21:45:12 -0300

    web: init

---
+++ apps/web/src/app.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@printer-center/ui-components';
+
+export function App() {
+  return (
+    <div>
+      <Button>Content</Button>
+    </div>
+  );
+}
+
+export default App;
